cc4b7b730fbe1f07c858a98e941808f7bdd66962
Andrew <andrewsabatyuk17@gmail.com>
2021-02-02 21:54:34 +0200

    v 1.02
    create navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Navbar = () => {
-    return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
-    )
+	const [click, setClick] = useState(false);
+
+
+	const handleClick = () => setClick(!click);
+	const closeMobileMenu = () => setClick(false);
+return (
+  <>
+      <nav className='navbar'>
+         <div className='navbar-container'>
+					 <a href='#' className='navbar-logo'>
+						 TRVL <i class='fab fa-typo3' />
+					 </a>
+					 <div className='menu-icon' onClick={handleClick}>
+						 <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+					 </div>
+					 <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+						 <li className='nav-item'>
+							 <a href='#' className='nav-links' onClick={closeMobileMenu}>Home</a>
+						 </li>
+						 <li className='nav-item'>
+							 <a href='Services' className='nav-links' onClick={closeMobileMenu}>Services</a>
+						 </li>
+						 <li className='nav-item'>
+							 <a href='Products' className='nav-links' onClick={closeMobileMenu}>Products</a>
+						 </li>
+						 <li className='nav-item'>
+							 <a href='sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>Sign-Up</a>
+						 </li>
+					 </ul>
+				 </div> 
+      </nav>
+  </>
+)
 }
 
 export default Navbar
