5e42e298c5494c9defbff0894575ef412b307e80
Андрій <64520200+Andrewsabatyuk@users.noreply.github.com>
2021-02-03 15:22:37 +0200

    v 1.04
    finish navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {Button} from './Button'
 import './Navbar.css'
 
@@ -16,13 +16,18 @@ const showButton =() =>{
 		setButton(true)
 	}
 }
+
+useEffect(() =>{
+	showButton()
+}, [])
+
 window.addEventListener('resize', showButton)
 
 return (
 	<>
 		<nav className='navbar'>
 			<div className='navbar-container'>
-				<a href='#' className='navbar-logo'>
+				<a href='#' className='navbar-logo' onClick={closeMobileMenu}>
 					TRVL <i class='fab fa-typo3' />
 				</a>
 				<div className='menu-icon' onClick={handleClick}>
